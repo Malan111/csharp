31140880f6d4709b04a7c6d5e2a43af1d658db20
Malan111 <45174067+Malan111@users.noreply.github.com>
2020-03-09 15:11:34 +0300

    Add files via upload

---
+++ task1/SRC/task1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+
+        static string itoBase(long nb, string basu)
+        {
+            string a = "";
+            int b = Convert.ToInt32(basu);
+            long c;
+            
+
+            while (nb != 0)
+            {
+                c = nb % b;
+                nb /= b;
+
+                if (c > 9 && c < 16)
+                {
+                    //Если 'c'= 10/11/12/13/14/15 то 'c'=a/b/c/d/e/f;
+                    switch (c)
+                    {
+                        case 10:
+                            a = a.Insert(0, "a");
+                            break;
+                        case 11:
+                            a = a.Insert(0, "b");
+                            break;
+                        case 12:
+                            a = a.Insert(0, "c");
+                            break;
+                        case 13:
+                            a = a.Insert(0, "d");
+                            break;
+                        case 14:
+                            a = a.Insert(0, "e");
+                            break;
+                        case 15:
+                            a = a.Insert(0, "f");
+                            break;
+                    }
+                }
+                else
+                     a = a.Insert(0, c.ToString());
+            }
+
+            return a;
+        }
+        
+        static string itoBase(string nb, string baseSrc, string baseDst)
+        {
+            string a = "";
+            int b = Convert.ToInt32(baseSrc);
+            int b_1 = Convert.ToInt32(baseDst);
+            double pow, _nb = 0;
+            long c;
+            long num = Convert.ToInt64(nb);
+
+
+
+
+            //Перевод из baseSrc -> 10
+            for (int i = 0; i < nb.Length; i++)  {
+                pow = Math.Pow(b, i);
+                _nb += (num % 10) * pow;
+                num /= 10;
+            }
+
+            long nb_1 = Convert.ToInt64(_nb);
+
+            //Из 10 -> baseDst
+                while (nb_1 != 0)
+                {
+                    c = nb_1 % b_1;
+                    nb_1 /= b_1;
+
+                    if (c > 9 && c < 16)
+                    {
+                        //Если 'c'= 10/11/12/13/14/15 то 'c'=a/b/c/d/e/f;
+                        switch (c)
+                        {
+                            case 10:
+                                a = a.Insert(0, "a");
+                                break;
+                            case 11:
+                                a = a.Insert(0, "b");
+                                break;
+                            case 12:
+                                a = a.Insert(0, "c");
+                                break;
+                            case 13:
+                                a = a.Insert(0, "d");
+                                break;
+                            case 14:
+                                a = a.Insert(0, "e");
+                                break;
+                            case 15:
+                                a = a.Insert(0, "f");
+                                break;
+                        }
+                    }
+                    else
+                        a = a.Insert(0, c.ToString());
+                }
+
+            return a;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            try{
+                Console.Write("Введите число в 10 СИ: ");
+                long a = Convert.ToInt64(Console.ReadLine());
+                Console.Write("Введите СИ: ");
+                string str = Console.ReadLine();
+
+                Console.WriteLine(itoBase(a, str));
+
+                Console.WriteLine();
+                Console.WriteLine("Дополнительно:");
+
+
+
+                string num, strF, strL;
+
+                Console.Write("Введите число: ");
+                num = Console.ReadLine();
+                Console.Write("Из: ");
+                strF = Console.ReadLine();
+                Console.Write("В: ");
+                strL = Console.ReadLine();
+
+                Console.WriteLine(itoBase(num, strF, strL));
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("usage");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task4/SRC/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str_1 = Console.ReadLine();
+            int strF = str.Length;
+            int strL = str_1.Length;
+            int symbol = 0, k = 0, l = 0, m = 1, n = 0;
+
+            if (str == str_1)
+                Console.WriteLine("OK");
+            else
+            {
+                //Поиск '*'
+                for (int i = 0; i < strL; i++)
+                {
+                    if (str_1[i] == '*')
+                    {
+                        symbol++;
+                    }
+                }
+
+                if (symbol == strL)
+                    Console.WriteLine("OK");
+
+                else
+                {
+                    //Поиск индекса последней совпашего символа
+                    for (int i = 0; i < strL; i++)
+                    {
+                        for (int j = 0; j < strF; j++)
+                        {
+                            if (str_1[i] == str[j])
+                            {
+                                k = i;
+                                l++;
+                                break;
+                            }
+                        }
+                        if (str_1[i] == str[strF - 1])
+                            break;
+                    }
+
+                    //Перед совпавшим элементом
+                    for (int i = 0; i < k; i++)
+                    {
+                        if (str_1[i] != '*')
+                        {
+                            for (int j = 0; j < strF; j++)
+                            {
+                                if (str_1[i] == str[j])
+                                {
+                                    m++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = k; i < strL; i++)
+                    {
+                        if (str_1[i] != '*')
+                            n++;
+                    }
+
+                    if (n == 1 && m == l)
+                        Console.WriteLine("OK");
+                    else
+                        Console.WriteLine("KO");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
